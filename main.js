88881c471c28361866ab4b65d6b1a3c41d16b70d
Smrithi2714 <97660148+Smrithi2714@users.noreply.github.com>
2022-05-31 12:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Hi i am console screen");
+var first_name ="Smrithi";
+console.log (first_name);
+var last_name ="Elizabeth";
+console.log (last_name);
+var full_name = first_name.concat (last_name);
+console.log(full_name);
